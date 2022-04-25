6246393431614adfcc6aa6d065ddfc37b846be4c
LoiseMulwa <loisemulwa7@gmail.com>
2022-04-25 17:55:03 +0300

    ft: initial commits

---
+++ Loise-Mulwa/test.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def landingpage():
+    # data="Flask is a Microframework"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/facts')
+def contact():
+    return render_template('facts.html')
+
+if __name__=='__main__':
+    app.run(debug=1)
