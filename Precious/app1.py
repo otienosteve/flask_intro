77ea0b34cf2825578fd5387b3b6352c2f7df98f0
halima precious <halimaprecious3@gmail.com>
2022-04-25 17:24:11 +0300

    last commit

--- Precious/app1.py
+++ Precious/app1.py
@@ -9,7 +9,13 @@ def homepage():
 def about():
     return render_template("about.html")
 
+@app.route('/services')
+def services():
+    return render_template("services.html")
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 if __name__=='__main__':
     app.run(debug=1)
