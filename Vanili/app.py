d6309310445ae6b01359e9e4c967e6c33ef14abb
Vanili <katevanili98@gmail.com>
2022-04-25 16:07:45 +0300

    changed

--- Vanili/app.py
+++ Vanili/app.py
@@ -4,6 +4,7 @@ from datetime import datetime  # add
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # add
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)  # add
 
 
