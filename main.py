748dbfee3aada9e8a654e0a0f32036baa28f0932
otienosteve <steveotieno701@gmail.com>
2022-04-25 10:27:40 +0300

    flask intro

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    data="Flask is a Microframework"
+    return render_template('index.html',datum=data)
+
+@app.route('/about')
+def about():
+    return "<h1>This is the About Page<h1>"
+
+
+@app.route('/serve')
+def services():
+    return "<h2>Services<h2>"
+
+if __name__=='__main__':
+    app.run(debug=1)
